5fb6cf1ed3418b4d1c8b641da55a30fb1395a5d8
Dominique Caracciolo <dominique.caracciolo@laplateforme.io>
2023-04-24 10:25:14 +0200

    Class and Object

---
+++ jour01/job01/Operation.py
@@ -0,0 +1,8 @@
+class Operation:
+    def __init__(self):
+        self.number1= 1
+        self.number2= 2
+
+operation = Operation()
+
+print(operation)
---
+++ jour01/job02/operation.py
@@ -0,0 +1,19 @@
+class Operation:
+    def __init__(self):
+        self.number1= 12
+        self.number2= 3
+
+    def show_number1(self):
+        print("le Nombre 1 est : " + str(self.number1))
+    
+    def show_number2(self):
+        print("le Nombre 1 est : " + str(self.number2))
+
+
+
+
+operation = Operation()
+
+#print(operation)
+operation.show_number1()
+operation.show_number2()
