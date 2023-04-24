b08fef5f09f228d24e4e270b05d2157f22f55712
Dominique Caracciolo <dominique.caracciolo@laplateforme.io>
2023-04-24 22:06:08 +0200

    Last push

--- jour01/job08/Livre.py
+++ jour01/job08/Livre.py
@@ -14,10 +14,8 @@ class Livre:
         return self.__auteur 
     def get_pages(self):
         print("Le nombre de pages est de", self.__pages)
-
         return self.__pages
     
-
     def set_values(self, set_titre,set_auteur, set_pages):
         self.__titre = set_titre
         self.__auteur = set_auteur
@@ -27,15 +25,53 @@ class Livre:
             self.__pages = self.__pages
             print("Le nombre de pages doit etre positif, sans decimales,la valeur du nombre de pages n'est pas changer")
         
+    def verification(self, verif):
+        if self.__disponible is True:
+            verif == True
+        else: 
+            verif == False
+
+    def get_disponible(self):
+        #print(self.__disponible)
+        return self.__disponible
+        
+
+    def set_disponible(self,dispo):
+        self.__disponible = dispo
+
+    def emprunter(self,borrow):
+        if borrow == True :
+            self.__disponible = False
+            print("Le livre est emprunté")
+            return True
+        else:
+            return False
+            
+    def rendre(self, giveback):
+        if self.__disponible == False:
+            self.__disponible = True
+            print("Le livre est rendu")
+        else:
+            print("Le livre n'est pas emprunté, il ne peut pas être rendu")
 
 
 livre = Livre()
-livre.get_titre()
-livre.get_auteur()
-livre.get_pages()
-print("---------------------------------")
-livre.set_values("Le nouveau nom du livre", "Le nouvel auteur du livre", 25)
 
-livre.get_titre()
-livre.get_auteur()
-livre.get_pages()   
+print(livre.get_disponible())
+livre.emprunter(True) 
+print(livre.get_disponible())
+livre.rendre(True)
+print(livre.get_disponible())
+
+
+
+
+# livre.get_titre()
+# livre.get_auteur()
+# livre.get_pages()
+# print("---------------------------------")
+# livre.set_values("Le nouveau nom du livre", "Le nouvel auteur du livre", 25)
+
+# livre.get_titre()
+# livre.get_auteur()
+# livre.get_pages()   
